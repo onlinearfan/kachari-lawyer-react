96023ca64b47f165b2cb82192c8fffe103dc4596
Md Arfan Khan <khan188993@gmail.com>
2022-04-17 23:07:16 +0600

    login with custom email and password done

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -6,14 +6,17 @@ import {
     GoogleAuthProvider,
     signInWithPopup,
 } from "firebase/auth";
+import auth from "../../Firebase/Firebase.init";
 
 const LoginPage = () => {
         const navigate = useNavigate();
         const [email, setEmail] = useState({ value: "", error: "" });
         const [password, setPassword] = useState({ value: "", error: "" });
 
+        
         //Google Auth 
         const googleAuth = () => {
+        const provider = new GoogleAuthProvider();
             signInWithPopup(auth, provider)
               .then((result) => {
                 const user = result.user;
@@ -46,10 +49,37 @@ const LoginPage = () => {
             setPassword({ value: passwordInput, error: "" });
           };
 
-        //handle login form 
-        const handleLogin = (e) =>{
-            e.preventDefault();
-        }
+        //handle login form with custom email and password
+        const handleLogin = (event) => {
+            event.preventDefault();
+        
+            if (email.value === "") {
+              setEmail({ value: "", error: "Email is required" });
+            }
+        
+            if (password.value === "") {
+              setPassword({ value: "", error: "Password is required" });
+            }
+        
+            if (email.value && password.value) {
+              signInWithEmailAndPassword(auth, email.value, password.value)
+                .then((userCredential) => {
+                  const user = userCredential.user;
+                  console.log(user);
+                  toast.success("Sign In Successfully", { id: "signIn" });
+                  navigate("/");
+                })
+                .catch((error) => {
+                  const errorMessage = error.message;
+        
+                  if (errorMessage.includes("wrong-password")) {
+                    toast.error("Wrong Password", { id: "error" });
+                  } else {
+                    toast.error(errorMessage, { id: "error" });
+                  }
+                });
+            }
+          };
     return (
         <div className="testimonial-and-quote-area pt-100 pb-80">
 		    <div className="container">
@@ -62,19 +92,21 @@ const LoginPage = () => {
 		                    <div className="request-quate-form">
 		                        <form action="#" onSubmit={handleLogin}>
 		                            <div className="row">
-		                                <div className="col-12">
+                                    <div className="col-12">
 		                                    <div className="request-quate-form-style mb-30">
-		                                        <input name="email" placeholder="Email" type="email"/>
+		                                        <input onBlur={handleEmail} name="email" id="email" placeholder="Email" type="email"/>
+                                                {email.error && (<span className="error">{email.error}</span>)}
 		                                    </div>
 		                                </div>
 		                                <div className="col-12">
 		                                    <div className="request-quate-form-style mb-30">
-		                                        <input name="password" placeholder="Password" type="password"/>
+		                                        <input onBlur={handlePassword} name="password" id="password" placeholder="Password" type="password"/>
+                                                {password.error && (<span className="error">{password.error}</span>)}
 		                                    </div>
 		                                </div>
 		                                <div className="col-md-12 col-lg-12">
 		                                    <div className="request-quate-form-style">
-                                            <button type="submit" className="btn">Log In</button>
+                                            <button onClick={handleLogin} type="submit" className="btn">Log In</button>
 		                                    </div>
 		                                </div>
 		                            </div>
