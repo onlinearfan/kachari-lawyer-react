df79928fd7fce8cdcb44ac15093f478f1ceac842
Md Arfan Khan <khan188993@gmail.com>
2022-04-18 00:12:03 +0600

    not found page 404 added

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = () => {
       <Route path="checkout" element={<Checkout/>}/>
       <Route path="login" element={<LoginPage/>}/>
       <Route path="signup" element={<SignUpPage/>}/>
+      <Route path='*' element={<NotFound />} />
      
     </Routes>
     </>
