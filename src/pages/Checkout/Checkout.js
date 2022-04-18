aa412fd96540bab323cc0530d756f01258dd8593
Md Arfan Khan <khan188993@gmail.com>
2022-04-18 12:31:47 +0600

    checkout page form verification added

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useLocation,useNavigate } from 'react-router-dom';
 import serviceData from '../../fakeData/service-data';
 import toast from 'react-hot-toast';
 
 const Checkout = () => {
+	const [name, setName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
+	const [error,setError] = useState(false);
     //Checkout Product Id will be here;
     const {state} = useLocation();
 	const navigate = useNavigate();
@@ -11,11 +15,17 @@ const Checkout = () => {
 	const checkoutProduct = serviceData.find((v)=>v.id===state.id);
 	const handleCheckout = (e)=>{
 		e.preventDefault();
-		toast.success("Thanks for ordering our service", { id: "order" });
-        navigate("/");
+		if(email && phone && name){
+			setError(false)
+			toast.success("Thanks for ordering our service", { id: "order" });
+        	navigate("/");
+		}else{
+			setError(true)
+		}
+		
 	}
 
-	console.log(checkoutProduct);
+	console.log(name,phone,email);
     return (
         <div className="testimonial-and-quote-area pt-100 pb-80">
 		    <div className="container">
@@ -30,23 +40,24 @@ const Checkout = () => {
 		                            <div className="row">
 		                                <div className="col-12">
 		                                    <div className="request-quate-form-style mb-30">
-		                                        <input name="name" placeholder="Name" type="text"/>
+		                                        <input required onBlur={(e)=>setName(e.target.value)} name="name" placeholder="Name" type="text"/>
 		                                    </div>
 		                                </div>
 		                                <div className="col-12">
 		                                    <div className="request-quate-form-style mb-30">
-		                                        <input name="phone" placeholder="Phone" type="text"/>
+		                                        <input required onBlur={(e)=>setPhone(e.target.value)} name="phone" placeholder="Phone" type="text"/>
 		                                    </div>
 		                                </div>
 		                                <div className="col-12">
 		                                    <div className="request-quate-form-style mb-30">
-		                                        <input name="email" placeholder="Email" type="email"/>
+		                                        <input required onBlur={(e)=>setEmail(e.target.value)} name="email" placeholder="Email" type="email"/>
 		                                    </div>
 		                                </div>
 		                                <div className="col-md-12 col-lg-12">
 		                                    <div className="request-quate-form-style">
 		                                        <button onClick={handleCheckout} type="submit" className="default-btn">Order<i className="icofont icofont-long-arrow-right"></i></button>
 		                                    </div>
+											{error && <span>Please Give your details properly for a successful order.</span>}
 		                                </div>
 		                            </div>
 		                        </form>
